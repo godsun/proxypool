e0ce547aa5f5b401fa470f113ec50cd7f516a576
konbar <22516445+konbar@users.noreply.github.com>
2021-02-19 14:15:08 +0800

    Add files via upload

--- internal/cron/cron.go
+++ internal/cron/cron.go
@@ -1,14 +1,14 @@
 package cron
 
 import (
-	"github.com/Sansui233/proxypool/config"
-	"github.com/Sansui233/proxypool/internal/cache"
-	"github.com/Sansui233/proxypool/log"
-	"github.com/Sansui233/proxypool/pkg/healthcheck"
-	"github.com/Sansui233/proxypool/pkg/provider"
+	"github.com/godsun/proxypool/config"
+	"github.com/godsun/proxypool/internal/cache"
+	"github.com/godsun/proxypool/log"
+	"github.com/godsun/proxypool/pkg/healthcheck"
+	"github.com/godsun/proxypool/pkg/provider"
 	"runtime"
 
-	"github.com/Sansui233/proxypool/internal/app"
+	"github.com/godsun/proxypool/internal/app"
 	"github.com/jasonlvhit/gocron"
 )
 
